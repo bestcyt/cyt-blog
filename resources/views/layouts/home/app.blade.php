c50961fb245364d55a7ac2ed1af7db7ef9431162
USER-20160304GT\Administrator <yitichen22@gmail.com>
2017-08-30 16:32:10 +0800

    xx

--- resources/views/layouts/home/app.blade.php
+++ resources/views/layouts/home/app.blade.php
@@ -8,11 +8,12 @@
 <head>
     <meta charset="utf-8">
     <link rel="icon" href=" {{ base_path('bitbug_favicon.png') }}" type="image/x-icon"/>
+    {{--<link rel="icon" href=" {{ asset('bitbug_favicon.png') }}" type="image/x-icon"/>--}}
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{ config('blog_config.blog_title.0') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Free HTML5 Website Template by GetTemplates.co" />
-    <meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+    <meta name="description" content="cyt-blog" />
+    <meta name="keywords" content="cyt-blog" />
     <meta name="author" content="GetTemplates.co" />
 
     <!-- Facebook and Twitter integration -->
